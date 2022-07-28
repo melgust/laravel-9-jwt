fc7820d667c04581ca7a6e1e895429d73b1322e2
Gustavo Cali <melgust@gmail.com>
2022-07-28 10:33:33 -0600

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#API with Laravel 9
+
+Base from https://www.positronx.io/laravel-jwt-authentication-tutorial-user-login-signup-api/
+
+Commands
+
+composer require tymon/jwt-auth --ignore-platform-reqs
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json([
+            'products' => $products
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+
+        return response()->json([
+            'message' => "Product saved successfully!",
+            'product' => $product
+        ], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Product $product)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        $product->update($request->all());
+
+        return response()->json([
+            'message' => "Product updated successfully!",
+            'product' => $product
+        ], 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+        return response()->json([
+            'message' => "Product deleted successfully!",
+        ], 200);
+    }
+}
